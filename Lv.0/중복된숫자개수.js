a0c9ad904aa5fa4c42d6a56350dd60cf33498041
saerom <sagesaeromkim@gmail.com>
2023-02-11 14:41:50 +0900

    2/11

---
+++ Lv.0/중복된숫자개수.js
@@ -0,0 +1,21 @@
+// 중복된 숫자 개수
+// 문제 설명
+// 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 1 ≤ array의 길이 ≤ 100
+// 0 ≤ array의 원소 ≤ 1,000
+// 0 ≤ n ≤ 1,000
+
+function solution(array, n) {
+    var answer = 0;
+    let Array = array.filter((item) => item === n)
+    answer = Array.length
+
+    return answer;
+}
+
+// 다른 사람의 풀이
+function solution(array, n) {
+    return array.reduce((a,b) => n === b ? a+1 : a,0)
+}
