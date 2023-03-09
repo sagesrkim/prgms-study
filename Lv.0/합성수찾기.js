517a5ac551848b2c540735cf476d5e61881d2e8f
saerom <sagesaeromkim@gmail.com>
2023-03-09 11:50:37 +0900

    3/9 sqrt

--- Lv.0/합성수찾기.js
+++ Lv.0/합성수찾기.js
@@ -14,3 +14,23 @@ function solution(n) {
   }
   return arr.size;
 }
+
+// 다른 풀이
+
+function solution(n) {
+  let result = 0;
+
+  // 소수 찾기
+  function isPrime(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return true;
+    }
+    return false;
+  }
+
+  for (let i = 0; i <= n; i++) {
+    if (isPrime(i)) result++;
+  }
+
+  return result;
+}
