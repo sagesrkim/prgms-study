fde4a8c21959f475d45d7360c3d551c0371ddbc1
saerom <sagesaeromkim@gmail.com>
2023-03-19 23:27:30 +0900

    3/19 내림차순순sort,map,indexOf

---
+++ Lv.0/진료순서.js
@@ -0,0 +1,21 @@
+// 진료 순서 정하기
+// 문제 설명
+// 외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다. 정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 중복된 원소는 없습니다.
+// 1 ≤ emergency의 길이 ≤ 10
+// 1 ≤ emergency의 원소 ≤ 100
+
+// 배열 복사, 내림차순 정렬, 문자열 찾기
+function solution(emergency) {
+  const rank = [...emergency];
+  rank.sort((a, b) => b - a);
+  return emergency.map((n) => rank.indexOf(n) + 1);
+}
+
+// 다른 풀이
+function solution(emergency) {
+  let sorted = emergency.slice().sort((a, b) => b - a);
+  return emergency.map((v) => sorted.indexOf(v) + 1);
+}
