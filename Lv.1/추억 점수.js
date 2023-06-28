6faf408889f7a4a743e1cb02e35ed6b3300d29ef
saerom <sagesaeromkim@gmail.com>
2023-06-28 22:40:03 +0900

    6/28

---
+++ Lv.1/추억 점수.js
@@ -0,0 +1,38 @@
+// 추억 점수
+// 문제 설명
+// 사진들을 보며 추억에 젖어 있던 루는 사진별로 추억 점수를 매길려고 합니다. 사진 속에 나오는 인물의 그리움 점수를 모두 합산한 값이 해당 사진의 추억 점수가 됩니다. 예를 들어 사진 속 인물의 이름이 ["may", "kein", "kain"]이고 각 인물의 그리움 점수가 [5점, 10점, 1점]일 때 해당 사진의 추억 점수는 16(5 + 10 + 1)점이 됩니다. 다른 사진 속 인물의 이름이 ["kali", "mari", "don", "tony"]이고 ["kali", "mari", "don"]의 그리움 점수가 각각 [11점, 1점, 55점]]이고, "tony"는 그리움 점수가 없을 때, 이 사진의 추억 점수는 3명의 그리움 점수를 합한 67(11 + 1 + 55)점입니다.
+
+// 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning, 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 매개변수로 주어질 때, 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return하는 solution 함수를 완성해주세요.
+
+function solution(name, yearning, photo) {
+  let info = {};
+
+  name.forEach((person, index) => {
+    info[person] = yearning[index];
+  });
+
+  let ans = [];
+
+  for (let i = 0; i < photo.length; i++) {
+    const image = photo[i];
+
+    let sum = 0;
+
+    for (let j = 0; j < image.length; j++) {
+      if (info[image[j]]) {
+        sum += info[image[j]];
+      }
+    }
+
+    ans.push(sum);
+  }
+
+  return ans;
+}
+
+// 다른 풀이 - reduce안에서는 indexOf로 name에서 해당 사람의 인덱스를 가져온 후, yearning에 데이터로 가져온다. 다만 점수 데이터가 없으면 undefined가 반환되므로 0으로 처리. 그걸 합산해서 반환한다.
+function solution(name, yearning, photo) {
+  return photo.map((v) =>
+    v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0), 0)
+  );
+}
