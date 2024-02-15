f2028e2d901d26e507be1b3ae998ed440e0a70ec
saerom <sagesaeromkim@gmail.com>
2024-02-15 22:16:07 +0900

    240215

---
+++ Lv.0/최빈값 구하기.js
@@ -0,0 +1,9 @@
+// 문제 설명
+// 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
+
+function solution(array) {
+  let m = new Map();
+  for (let n of array) m.set(n, (m.get(n) || 0) + 1);
+  m = [...m].sort((a, b) => b[1] - a[1]);
+  return m.length === 1 || m[0][1] > m[1][1] ? m[0][0] : -1;
+}
